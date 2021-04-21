3902a90e16dafeb629724e02b634224ed780227c
Eliana Christou <echris15@uncc.edu>
2021-04-21 11:43:07 -0500

    read the data in the R fild

--- R/Round1_pred.R
+++ R/Round1_pred.R
@@ -2,4 +2,9 @@
 path = 'data/Data_2002_19.rda'
 load(path)
 
+x = rnorm(100)
+y = rnorm(100)
 
+y <- Data_2002_19$rd_1[which(Data_2002_19[, 2]<="2016")]
+
+# quantreg::rq(y~x) # Every time you use a package, you need to import it in DESCRIPTION

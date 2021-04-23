7930e67cc2e3bd5500d28cf01d7177f069fc137f
krmays <kmays4@uncc.edu>
2021-04-23 09:47:25 -0400

    Added data and column descriptions

--- R/data.R
+++ R/data.R
@@ -2,51 +2,51 @@
 #'
 #' Description
 #'
-#' @source \doi{test}
+#' @source \doi{team-wide statistics organized by team and season from teamrankings.com}
 #'
 #' @format A data frame with the following columns:
 #'
 #' \describe{
 #'
-#' \item{team}{test}
-#' \item{year}{test}
-#' \item{region}{test}
-#' \item{seed}{test2}
-#' \item{play.in}{...}
-#' \item{rd_1}{...}
-#' \item{rd_2}{...}
-#' \item{rd_3}{...}
-#' \item{rd_4}{...}
-#' \item{rd_5}{...}
-#' \item{rd_6}{...}
-#' \item{TPPG}{...}
-#' \item{TPPG_last3}{...}
-#' \item{FGPG}{...}
-#' \item{FGPG_last3}{...}
-#' \item{FTAPG}{...}
-#' \item{FTAPG_last3}{...}
-#' \item{FTMPP}{...}
-#' \item{FTMPP_last3}{...}
-#' \item{ORP}{...}
-#' \item{ORP_last3}{...}
-#' \item{ORPG}{...}
-#' \item{ORPG_last3}{...}
-#' \item{DRP}{...}
-#' \item{DRP_last3}{...}
-#' \item{DRPG}{...}
-#' \item{DRPG_last3}{...}
-#' \item{APG}{...}
-#' \item{APG_last3}{...}
-#' \item{FPG}{...}
-#' \item{FPG_last3}{...}
-#' \item{ASM}{...}
-#' \item{ASM_last3}{...}
-#' \item{ATR}{...}
-#' \item{ATR_last3}{...}
-#' \item{OE}{...}
-#' \item{OE_last3}{...}
-#' \item{DE}{...}
-#' \item{DE_last3}{...}
+#' \item{team}{team name}
+#' \item{year}{tournament year}
+#' \item{region}{tournament region}
+#' \item{seed}{tournament seed}
+#' \item{play.in}{results of play-in games}
+#' \item{rd_1}{results of round 1 games}
+#' \item{rd_2}{results of round 2 games}
+#' \item{rd_3}{results of round 3 games}
+#' \item{rd_4}{results of round 4 games}
+#' \item{rd_5}{results of round 5 games}
+#' \item{rd_6}{results of round 6 games}
+#' \item{TPPG}{three pointers scored per game (season average)}
+#' \item{TPPG_last3}{three pointers scored per game (last 3 game average)}
+#' \item{FGPG}{field goals scored per game (season average)}
+#' \item{FGPG_last3}{field goals scored per game (last 3 game average)}
+#' \item{FTAPG}{free throws attempted oer game (season average)}
+#' \item{FTAPG_last3}{free throws attempted oer game (last 3 game average)}
+#' \item{FTMPP}{free throws made per 100 possessions (season average)}
+#' \item{FTMPP_last3}{free throws made per 100 possessions (last 3 game average)}
+#' \item{ORP}{offensive rebound percentage (season average)}
+#' \item{ORP_last3}{offensive rebound percentage (last 3 game average)}
+#' \item{ORPG}{offensive rebounds per game (season average)}
+#' \item{ORPG_last3}{offensive rebounds per game (last 3 game average)}
+#' \item{DRP}{defensive rebound percentage (season average)}
+#' \item{DRP_last3}{defensive rebound percentage (last 3 game average)}
+#' \item{DRPG}{defensive rebounds per game (season average)}
+#' \item{DRPG_last3}{defensive rebounds per game (last 3 game average)}
+#' \item{APG}{assists per game (season average)}
+#' \item{APG_last3}{assists per game (last 3 game average)}
+#' \item{FPG}{fouls per game (season average)}
+#' \item{FPG_last3}{fouls per game (last 3 game average)}
+#' \item{ASM}{average scoring margin (season average)}
+#' \item{ASM_last3}{average scoring margin (last 3 game average)}
+#' \item{ATR}{assists to turnover ratio (season average)}
+#' \item{ATR_last3}{assists to turnover ratio (last 3 game average)}
+#' \item{OE}{offensive efficiency (season average)}
+#' \item{OE_last3}{offensive efficiency (last 3 game average)}
+#' \item{DE}{defensive efficiency (season average)}
+#' \item{DE_last3}{defensive efficiency (last 3 game average)}
 #'
 #' }
 #'

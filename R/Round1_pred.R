e35ae11039d54164981982a263cedb1ee0116579
Eliana Christou <echris15@uncc.edu>
2021-04-24 18:56:57 -0500

    clean the code - define train and testing data sets, run cqs function, and calculate the probabilities

--- R/Round1_pred.R
+++ R/Round1_pred.R
@@ -1,55 +1,50 @@
+#' @include bic_d.R
+
 # Predicting the outcome of tournaments - Round 1
 path = 'data/Data_2002_19.rda'
 load(path)
-Data_2002_19
 
-# set variables (season-long stats only)
-y <- Data_2002_19$rd_1[which(Data_2002_19[, 2]<="2016")]
-length(y)
-x1 <- Data_2002_19[which(Data_2002_19[, 2]<="2016"), c(4, seq(12, 38, by = 2))] # season stats
-dim(x1)
-summary(x1)
+year <- "2017"
 
-# define parameters
-n <- length(y)
-p1 <- dim(x1)[2]
+# define the data
+# x defines the season-long stats only
+y_train <- Data_2002_19$rd_1[which(Data_2002_19[, 2] <= as.character(as.numeric(year) - 1))]
+x_train <- Data_2002_19[which(Data_2002_19[, 2] <= as.character(as.numeric(year) - 1)),
+                        c(4, seq(12, 38, by = 2))]
+y_test <- Data_2002_19$rd_1[which(Data_2002_19[, 2] == "2017")]
+x_test <- Data_2002_19[which(Data_2002_19[, 2] == "2017"), c(4, seq(12, 38, by = 2))]
+
+x_train <- as.matrix(x_train)
+x_test <- as.matrix(x_test)
+
+# define the parameters
+n_train <- length(y_train)
+n_test <- length(y_test)
+p <- dim(x_train)[2]
 tau <- 0.1
+C = 2 # number of classification groups
+classes = c(0, 1) # classification groups
 
-# discretize y1
-y1 = y + .00001 * mean(y) * rnorm(n)
-summary(y1)
+# descretize y_train to be applicable to the cqs function
+y_train_dis <- y_train + .00001 * mean(y_train) * rnorm(n_train)
 
-# cqs at tau=0.1
-library(quantdr)
-out1 <- quantdr::cqs(x1, y1, tau = 0.1)
-out1
-plot(out1$qvalues)
-beta_hat1 <- cbind(out1$qvectors[, 1:out1$dtau])
-
-# define new sufficient predictors and test parameters
-new_data1 <- as.matrix(x1) %*% beta_hat1
-new_data1
-x_train <- x1
-y_train <- y1
-x_test <- Data_2002_19[which(Data_2002_19[, 2]=="2017"), c(4, seq(12, 38, by = 2))]
-y_test <- Data_2002_19$rd_1[which(Data_2002_19[, 2]=="2017")]
-nt = length(y_train); nte = length(y_test)
-C=2; classes=c(0,1)
-prob_results_CQS <- matrix(0,length(tau),nte)
-prob_results_reg <- matrix(0,length(tau),nte)
-
-# standardize predictors (should we change to use package MTS?)
-for (i in 1:p1){
-  x_train[,i]=(x_train[,i]-apply(x_train, 2, mean)[i])/apply(x_train, 2, sd)[i]
-  x_test[,i]=(x_test[,i]-apply(x_test, 2, mean)[i])/apply(x_test, 2, sd)[i]
+# apply the cqs function and perform dimension reduction
+out <- quantdr::cqs(x_train, y_train_dis, tau)
+dtau_hat <- out$dtau
+# the BBQ.grplasso algorithm requires at least two predictor variables
+if (dtau_hat < 2) {
+  dtau_hat = 2
 }
+beta_hat <- cbind(out$qvectors[, 1:dtau_hat])
 
-new_data_train_cqs = new_data1
-new_data_test_cqs = as.matrix(x_test)%*%beta_hat1
-# waiting on helping R file here (go ahead and test)
-fit_cqs <- BBQ.grplasso(y_train~new_data_train_cqs, tau, c(1,2), method='Binary', Run=1500, burn=500,Ce=0,scale=TRUE)
-cqs_prob<-BBQ.prob(fit_cqs,new_data_test_cqs)$p1x
-prob_results_cqs[j,]=cqs_prob
+# define the new sufficient predictors
+new_data_train <- x_train %*% beta_hat
+new_data_test <- x_test %*% beta_hat
+
+# run the classification
+fit <- BBQ.grplasso(y_train ~ new_data_train, tau, c(1, 2), method = 'Binary', Run = 1500,
+                    burn = 500, Ce = 0, scale = TRUE)
+prob_fit <- BBQ.prob(fit, new_data_test)$p1x
 
 
 
